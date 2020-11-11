d440557e2fb4a2db8664605a5158bfbf5da37813
deltagare <olusanyaabayomi@yahoo.com>
2020-11-11 12:28:57 +0100

    Commit Message for todo assignment

---
+++ src/main/java/se/lexicon/grouplnu/App.java
@@ -0,0 +1,14 @@
+package se.lexicon.grouplnu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/se/lexicon/grouplnu/AppTest.java
@@ -0,0 +1,20 @@
+package se.lexicon.grouplnu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
