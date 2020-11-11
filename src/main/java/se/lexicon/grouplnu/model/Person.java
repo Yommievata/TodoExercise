a67e74fc7e2387769599c38936c15264faf72135
deltagare <olusanyaabayomi@yahoo.com>
2020-11-11 13:48:05 +0100

    Part 2 and 3: Constructor, Getters and Setters

--- src/main/java/se/lexicon/grouplnu/model/Person.java
+++ src/main/java/se/lexicon/grouplnu/model/Person.java
@@ -2,10 +2,48 @@ package se.lexicon.grouplnu.model;
 
 public class Person {
 
-    int personIdInitial;
-    int personIdFinal;
-    String firstName = "";
-    String lastName = "";
+    private int personIdInitial;
+    private int personIdFinal;
+    private String firstName;
+    private String lastName;
 
+    //Constructor
+    public Person(int personIdInitial, int personIdFinal, String firstName, String lastName){
+        this.personIdInitial = personIdInitial;
+        this.personIdFinal = personIdFinal;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
+    public int getPersonIdInitial() {
+        return personIdInitial;
+    }
+
+    public void setPersonIdInitial(int personIdInitial) {
+        this.personIdInitial = personIdInitial;
+    }
+
+    public int getPersonIdFinal() {
+        return personIdFinal;
+    }
+
+    public void setPersonIdFinal(int personIdFinal) {
+        this.personIdFinal = personIdFinal;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
