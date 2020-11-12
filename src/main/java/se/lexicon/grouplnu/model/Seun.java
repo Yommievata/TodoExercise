0bfb9b7af655ce516ee18d607049ebac6f3ed7e7
Seun Bond <sadetoye@gmail.com>
2020-11-12 16:49:50 +0100

    testing

--- src/main/java/se/lexicon/grouplnu/model/Seun.java
+++ src/main/java/se/lexicon/grouplnu/model/Seun.java
@@ -2,7 +2,56 @@ package se.lexicon.grouplnu.model;
 
 public class Seun {
 
-    int accountBala = 12345;
+    //
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package se.lexicon.grouplnu.model;
+
+    public class Person {
+        private final int personId;
+        private String firstName;
+        private String lastName;
+
+    //to be deleted
+        public final int personId;
+        public String firstName;
+        public String lastName;
+
+
+
+        public Person(int personId, String firstName, String lastName) {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public int getPersonId() {
+            return this.personId;
+        }
+
+        public String getFirstName() {
+            return this.firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("\"Seun See this\" = Seun See this");
+        }
+    }
+
     String name = Yommie;
 
 }
