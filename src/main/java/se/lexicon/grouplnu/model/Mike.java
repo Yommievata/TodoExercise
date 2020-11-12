d4fe2ba8e55c5f08ffae8c195f159e5627e405fe
deltagare <olusanyaabayomi@yahoo.com>
2020-11-12 16:46:05 +0100

    mikey

---
+++ src/main/java/se/lexicon/grouplnu/model/Mike.java
@@ -0,0 +1,8 @@
+package se.lexicon.grouplnu.model;
+
+public class Mike {
+    public static void main(String[] args) {
+        String name = "Michael";
+        System.out.println("name = " + name);
+    }
+}
