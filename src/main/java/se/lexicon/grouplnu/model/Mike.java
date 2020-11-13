f5e28d2457f11b95269046175a47af96440baadc
Seun Bond <sadetoye@gmail.com>
2020-11-13 10:47:28 +0100

    Clean, tidy up.

--- src/main/java/se/lexicon/grouplnu/model/Mike.java
+++
@@ -1,8 +0,0 @@
-package se.lexicon.grouplnu.model;
-
-public class Mike {
-    public static void main(String[] args) {
-        String name = "Michael";
-        System.out.println("name = " + name);
-    }
-}
