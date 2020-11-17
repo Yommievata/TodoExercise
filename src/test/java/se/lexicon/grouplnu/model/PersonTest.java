99db3d938dee1cb0774283927035d24f80b2524a
deltagare <olusanyaabayomi@yahoo.com>
2020-11-17 19:03:01 +0100

    Person and todo sequencer done!

---
+++ src/test/java/se/lexicon/grouplnu/model/PersonTest.java
@@ -0,0 +1,36 @@
+package se.lexicon.grouplnu.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+      int personId;
+      String firstName;
+      String lastName;
+      Person person;
+
+    @Test
+    public void getPersonId() {
+
+    }
+
+
+    @Test
+    public void getFirstName() {
+
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+}
